7a339c37a54f9ce6bbc5fb457dd0aadc609fbee3
BrnMarq <brnmarq@gmail.com>
2026-04-15 14:19:40 -0400

    🔧 Updated main to run files and repl as well

--- src/main.go
+++ src/main.go
@@ -8,6 +8,18 @@ import (
 )
 
 func main() {
+	if len(os.Args) > 1 {
+		filename := os.Args[1]
+		file, err := os.Open(filename)
+		if err != nil {
+			fmt.Printf("Error opening file %s: %s\n", filename, err)
+			os.Exit(1)
+		}
+		defer file.Close()
+		repl.Run(file, os.Stdout)
+		return
+	}
+
 	user, err := user.Current()
 	if err != nil {
 		panic(err)
